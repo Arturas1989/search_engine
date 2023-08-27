c4746161ee9ebce4298c3f23598e06c646015d36
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2023-08-27 15:38:02 +0300

    Restructured html rendering + css

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 
 function FilterableProductTable(){
   return (
-    <>
+    <div className = "container">
       <SearchBar />
       <ProductTable />
-    </>
+    </div>
   )
 }
 
 function SearchBar(){
   return (
     <>
-      <label for="search">Search item</label><br></br>
+      <label htmlFor="search">Search item</label>
       <input type="search" id="search" name="search" placeholder="search"></input>
     </>
     
@@ -22,31 +22,34 @@ function SearchBar(){
 
 function ProductTable(){
   return (
-    <>
-      <tr>
-        <th>
-          Name
-        </th>
-        <th>
-          Price
-        </th>
-      </tr>
-      <ProductCategoryRow />
-        <ProductRow />
-        <ProductRow />
-        <ProductRow />
-      <ProductCategoryRow />
-        <ProductRow />
-        <ProductRow />
-        <ProductRow />
-    </>
+    <table>
+      <tbody>
+        <tr>
+          <th>
+            Name
+          </th>
+          <th>
+            Price
+          </th>
+        </tr>
+        <ProductCategoryRow />
+          <ProductRow />
+          <ProductRow />
+          <ProductRow />
+        <ProductCategoryRow />
+          <ProductRow />
+          <ProductRow />
+          <ProductRow />
+      </tbody>
+    </table>
+    
   )
 }
 
 function ProductCategoryRow(){
   return (
     <tr>
-      <td colSpan = "2">
+      <td className = "categoryRow" colSpan = "2">
           category
       </td>
     </tr>
