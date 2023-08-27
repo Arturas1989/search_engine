3d1fe86b0a7411b0969d512b8c678a3ee6af8899
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2023-08-27 14:39:05 +0300

    initial HTML structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
 
 import './App.css';
 
-function App() {
+function FilterableProductTable(){
   return (
-    <h1>This is a search engine app</h1>
-  );
+    <>
+      <SearchBar />
+      <ProductTable />
+    </>
+  )
 }
 
-export default App;
+function SearchBar(){
+  return (
+    <>
+      <label for="search">Search item</label><br></br>
+      <input type="search" id="search" name="search" placeholder="search"></input>
+    </>
+    
+  )
+}
+
+function ProductTable(){
+  return (
+    <>
+      <tr>
+        <th>
+          Name
+        </th>
+        <th>
+          Price
+        </th>
+      </tr>
+      <ProductCategoryRow />
+        <ProductRow />
+        <ProductRow />
+        <ProductRow />
+      <ProductCategoryRow />
+        <ProductRow />
+        <ProductRow />
+        <ProductRow />
+    </>
+  )
+}
+
+function ProductCategoryRow(){
+  return (
+    <tr>
+      <td colSpan = "2">
+          category
+      </td>
+    </tr>
+  )
+}
+
+function ProductRow(){
+  return (
+    <tr>
+      <td>
+        Item
+      </td>
+      <td>
+        Price
+      </td>
+    </tr>
+  )
+}
+
+export default FilterableProductTable;
